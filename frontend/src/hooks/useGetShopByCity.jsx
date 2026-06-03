c85929675fa8c1eeda61dbdc97958776f7d21977
airgeddon <chamancharsee@gmail.com>
2026-06-04 01:18:13 +0530

    Update useGetShopByCity.jsx
    Updated the shop fetching logic to use a global route instead of city-specific. Removed the dependency on currentCity.

--- frontend/src/hooks/useGetShopByCity.jsx
+++ frontend/src/hooks/useGetShopByCity.jsx
@@ -1,25 +1,27 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
 import { serverUrl } from '../App'
-import { useDispatch, useSelector } from 'react-redux'
-import { setShopsInMyCity, setUserData } from '../redux/userSlice'
+import { useDispatch } from 'react-redux'
+import { setShopsInMyCity } from '../redux/userSlice'
 
 function useGetShopByCity() {
-    const dispatch=useDispatch()
-    const {currentCity}=useSelector(state=>state.user)
-  useEffect(()=>{
-  const fetchShops=async () => {
-    try {
-           const result=await axios.get(`${serverUrl}/api/shop/get-by-city/${currentCity}`,{withCredentials:true})
-            dispatch(setShopsInMyCity(result.data))
-           console.log(result.data)
-    } catch (error) {
-        console.log(error)
-    }
-}
-fetchShops()
- 
-  },[currentCity])
+    const dispatch = useDispatch()
+    
+    // ✨ FIX: Removed currentCity dependency since we fetch globally now!
+    useEffect(() => {
+        const fetchShops = async () => {
+            try {
+                // ✨ FIX: Hitting the new global route
+                const result = await axios.get(`${serverUrl}/api/shop/get-all`, { withCredentials: true })
+                dispatch(setShopsInMyCity(result.data))
+                console.log("Fetched all shops globally:", result.data)
+            } catch (error) {
+                console.error("Fetch shops error:", error)
+            }
+        }
+        
+        fetchShops()
+    }, [dispatch]) 
 }
 
 export default useGetShopByCity
