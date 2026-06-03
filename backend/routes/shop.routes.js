33be56d75140f9bdd51cac8c94896b2b9760338d
airgeddon <chamancharsee@gmail.com>
2026-06-04 01:13:44 +0530

    Update shop.routes.js

--- backend/routes/shop.routes.js
+++ backend/routes/shop.routes.js
@@ -1,14 +1,15 @@
 import express from "express"
-import { createEditShop, getMyShop, getShopByCity } from "../controllers/shop.controllers.js"
+// ✨ IMPORTED the new getAllShops function
+import { createEditShop, getMyShop, getShopByCity, getAllShops } from "../controllers/shop.controllers.js"
 import isAuth from "../middlewares/isAuth.js"
 import { upload } from "../middlewares/multer.js"
-
-
 
 const shopRouter=express.Router()
 
 shopRouter.post("/create-edit",isAuth,upload.single("image"),createEditShop)
 shopRouter.get("/get-my",isAuth,getMyShop)
 shopRouter.get("/get-by-city/:city",isAuth,getShopByCity)
+// ✨ NEW ROUTE: The frontend will hit this to get every restaurant
+shopRouter.get("/get-all", isAuth, getAllShops)
 
 export default shopRouter
